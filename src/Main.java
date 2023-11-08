68408b308874c31af6c606f2c94adc2a712d3adf
aliona.binzari <alionab79@gmail.com>
2023-11-08 12:44:17 +0200

    Tema pe acasa, nr.14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Zoo zoo = new Zoo();
+
+        Elefant elefant = new Elefant("Elefantul Ram", 11);
+        elefant.adaugaAliment("iarba");
+        elefant.adaugaAliment("fructe");
+        zoo.adaugaAnimal(elefant);
+
+        Lup lup = new Lup("Lupul Ram", 3);
+        lup.adaugaAliment("carne");
+        zoo.adaugaAnimal(lup);
+
+        Leu leu = new Leu("Leul BOX", 5);
+        leu.adaugaAliment("carne");
+        zoo.adaugaAnimal(leu);
+
+        Jaguar jaguar = new Jaguar("Jaguarul Jaggy", 7);
+        jaguar.adaugaAliment("carne");
+        zoo.adaugaAnimal(jaguar);
+
+        Zebra zebra = new Zebra("Zebra Marta", 12);
+        zebra.adaugaAliment("iarba");
+        zoo.adaugaAnimal(zebra);
+
+        zoo.afiseazaAnimale();
+
     }
 }
--- src/ZOO.java
+++ src/ZOO.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class ZOO {
+import java.util.*;
+import java.util.ArrayList;
+
+// Clasa de baza Animal
+class Animal {
+    String nume;
+    int varsta;
+    List<String> alimente = new ArrayList<>();
+    private String aliment;
+
+    public Animal(String nume, int varsta) {
+        this.nume = nume;
+        this.varsta = varsta;
+    }
+
+    public void adaugaAliment(String aliment) {
+        this.aliment = aliment;
+        alimente.add(aliment);
+    }
+}
+
+// Clasa Elefant extinde Animal
+class Elefant extends Animal {
+    public Elefant(String nume, int varsta) {
+        super(nume, varsta);
+    }
+}
+
+// Clasa Lup extinde Animal
+class Lup extends Animal {
+    public Lup(String nume, int varsta) {
+        super(nume, varsta);
+    }
+}
+
+// Clasa Leu extinde Animal
+class Leu extends Animal {
+    public Leu(String nume, int varsta) {
+        super(nume, varsta);
+    }
+}
+
+// Clasa Jaguar extinde Animal
+class Jaguar extends Animal {
+    public Jaguar(String nume, int varsta) {
+        super(nume, varsta);
+    }
+}
+
+// Clasa Zebra extinde Animal
+class Zebra extends Animal {
+    public Zebra(String nume, int varsta) {
+        super(nume, varsta);
+    }
+}
+
+// Clasa Zoo
+class Zoo {
+    List<Animal> animale = new ArrayList<>();
+
+    public void adaugaAnimal(Animal animal) {
+        animale.add(animal);
+    }
+
+    public void afiseazaAnimale() {
+        for (Animal animal : animale) {
+            System.out.println("Nume: " + animal.nume);
+            System.out.println("Varsta: " + animal.varsta);
+            System.out.println("Prefera sa manance: " + animal.alimente);
+                     System.out.println();
+        }
+    }
 }
